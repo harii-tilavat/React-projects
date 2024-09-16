771c4384b4e1717ef37e34da5f558a4861e30b26
Harit tilavat <harittilavat10@gmail.com>
2024-09-16 15:59:31 +0530

    Added tailwind css basic

--- styling-react/src/components/AuthInputs.jsx
+++ styling-react/src/components/AuthInputs.jsx
@@ -3,13 +3,6 @@ import styled from "styled-components";
 import Button from "./Button";
 import Input from "./Input";
 
-const ControlerContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -31,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlerContainer>
+    <div id="auth-inputs" className="w-full max-w-md mx-auto p-8 rounded shadow bg-gradient-to-b from-stone-700 to-stone-800 ">
+      <div className="flex flex-col gap-2">
         {/* <p>
           <Label $invalid={emailNotValid}>Email</Label>
         </p> */}
@@ -51,9 +44,9 @@ export default function AuthInputs() {
             handleInputChange("password", event.target.value)
           }
         />
-      </ControlerContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end mt-4 gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
--- styling-react/src/components/Button.jsx
+++ styling-react/src/components/Button.jsx
@@ -1,17 +1,3 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
-export default Button;
+export default function Button({ children, ...props }) {
+  return <button className="font-semibold px-4 py-2 rounded-lg text-stone-900 bg-amber-500 hover:bg-amber-600" {...props}>{children}</button>;
+}
